4e6dc230f9dbbdcb5235e5508d99f0801e392d63
k3lly003 <ntirukelly@gmail.com>
2024-06-02 22:55:50 +0200

    nextjs & packages installations

---
+++ inventory/client/inventory-management/tailwind.config.ts
@@ -0,0 +1,81 @@
+import type { Config } from "tailwindcss";
+//this below color package is going to help us in switching mode colors
+import  { createThemes } from "tw-colors";
+import colors from "tailwindcss/colors";
+
+//colors am going to use with in the proje ct
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+//If the light mode is 'a' in dark mode will be 'b'
+const shadeMapping = {
+ // 'a': 'b'
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "500",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+
+//we gonna generate a theme Obj, we gonna pass all colors & shadeMapping, if invert = true, key : value (a:b)
+const generateThemeObject = (colors: any, mapping: any, invert = false) => {
+  const theme: any = {};
+  baseColors.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key, value]: any) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    });
+  });
+  return theme;
+};
+
+const lightTheme = generateThemeObject(colors, shadeMapping);
+const darkTheme = generateThemeObject(colors, shadeMapping, true);
+
+const themes = {
+  //light contains all lightTheme inside lightTheme
+  light: {
+    ...lightTheme,
+    white: "#ffffff",
+  },
+  //Dark contains all darkTheme inside darkTheme
+  dark: {
+    ...darkTheme,
+    white: colors.gray["950"],
+    black: colors.gray["50"],
+  },
+};
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [createThemes(themes)],
+};
+export default config;

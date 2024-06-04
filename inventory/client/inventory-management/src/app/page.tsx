48d70bda216afddfc18ed9478234201bb5c4cc48
k3lly003 <ntirukelly@gmail.com>
2024-06-04 21:39:43 +0200

    initial commit

--- inventory/client/inventory-management/src/app/page.tsx
+++ inventory/client/inventory-management/src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
-    </main>
-  );
+  return <Dashboard/>;
 }

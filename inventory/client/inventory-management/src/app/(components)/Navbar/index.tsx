bd242ae0f272cd0d5fd35e143392be6f1952cd82
k3lly003 <ntirukelly@gmail.com>
2024-06-08 13:13:59 +0200

    modified Side & Nav bar

--- inventory/client/inventory-management/src/app/(components)/Navbar/index.tsx
+++ inventory/client/inventory-management/src/app/(components)/Navbar/index.tsx
@@ -1,20 +1,32 @@
 'use client'
 import React from 'react'
-import { Menu, Bell, Sun, Settings } from 'lucide-react'
+import { Menu, Bell, Sun, Settings, Search } from 'lucide-react'
 import Link from 'next/link'
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  
+  const toogleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
   return (
     <div className='flex justify-between items-center w-full mb-7'>
         {/* LEFT SIDE */}
         <div className='flex justify-between items-center gap-5'>
-            <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100 onClick={()=>()}'>
+            <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={toogleSidebar}>
                 <Menu className='w-4 h-4'/>
             </button>
         </div>
         <div>
             <input type="search" placeholder='type to search ...' className='pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500' />
             <div className='absolute insert-y-0 left-0 pl-3 flex items-center pointer-events-non'>
-              {/* <Bell className='text-gray-500' size={20} /> */}
+              <Search className='text-gray-500' size={20} />
             </div>
         </div>
         {/* RIGHT SIDE */}
@@ -31,7 +43,7 @@ const Navbar = () => {
             </div>
             <hr className='w-0 h-7 border border-solid border-l border-gray-300 mx-3'/>
             <div className='flex items-center gap-3 cursor-pointer'>
-              <div className='w-9 h-9'>image</div>
+              <div className='w-9 h-9 p-5 bg-blue-100 rounded-full'></div>
               <span className='font-semibold'>Brice Ntiru</span>
             </div>
           </div>
--- inventory/client/inventory-management/src/app/(components)/SideBar/index.tsx
+++ inventory/client/inventory-management/src/app/(components)/SideBar/index.tsx
@@ -1,24 +1,39 @@
 'use client'
 
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
 import { Menu } from 'lucide-react'
 import React from 'react'
 
 type Props = {}
 
 const SideBar = (props: Props) => {
+  
+  const dispatch = useAppDispatch();
+  
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  
+  const toogleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarClassName = `fixed flex flex-col ${isSidebarCollapsed ? 'w-0 md:w-16' : 'w-72 md:w-64'} bg-white transition-all duration-500 overflow-hidden h-full shadow-md z-40`
+
   return (
-    <div>
+    <div className={sidebarClassName}>
         {/* TOP LOGO */}
-        <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+        <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? 'px-5' : 'px-8'}`}>
             <div>logo</div>
-            <h1 className='font-extrabold text-2xl'>NBSTOCK</h1>
-            <button className='md:hidden p-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={()=>{}}>
+            <h1 className={`${isSidebarCollapsed ? 'hidden' : 'block'} font-extrabold text-2xl`}>NBSTOCK</h1>
+            <button className='md:hidden p-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={toogleSidebar}>
             <Menu className='w-4 h-4' size={24}/>
         </button>
         </div>
         {/* LINKS */}
-        <div>
-
+        <div className='flex-grow mt-8'>
+          {/* {LINKS ....} */}
         </div>
         {/* FOOTER */}
         <div>

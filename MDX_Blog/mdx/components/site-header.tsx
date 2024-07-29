bb6bd9da34d7d9b7236cb5af91c50e74873b557c
k3lly003 <ntirukelly@gmail.com>
2024-07-29 09:43:48 +0200

    stage 4:Mobile Nav

--- MDX_Blog/mdx/components/site-header.tsx
+++ MDX_Blog/mdx/components/site-header.tsx
@@ -1,9 +1,10 @@
-import { siteConfig } from "@/config/site";
+ import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { Icons } from "./icons";
 import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
 // import { MobileNav } from "./mobile-nav";
 // import { ModeToggle } from "./mode-toggle";
 
@@ -60,7 +61,7 @@ export function SiteHeader() {
                 </div>
               </Link>
               {/* <ModeToggle /> */}
-              {/* <MobileNav /> */}
+              <MobileNav />
             </nav>
           </div>
         </div>

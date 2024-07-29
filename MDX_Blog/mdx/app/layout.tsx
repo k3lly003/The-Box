723592d7c7c0c1b941b6cee8adc3bd955abc6229
k3lly003 <ntirukelly@gmail.com>
2024-07-29 02:01:49 +0200

    stage3: site header

--- MDX_Blog/mdx/app/layout.tsx
+++ MDX_Blog/mdx/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { SiteHeader } from "@/components/site-header";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -17,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased")}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
+        <div className="relative flex min-h-dvh flex-col bg-background">
+          <SiteHeader/>
+          <main className="flex-1">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ MDX_Blog/mdx/config/site.ts
@@ -0,0 +1,16 @@
+import { Github, Linkedin } from "lucide-react";
+
+export const siteConfig  = {
+    name: "SheCanCode-Article",
+    url: "https://example.com",
+    description: "Nextjs 14 blog using mdx, velite, tailwindcss and shadcn",
+    author: "SheCanCode",
+    links: {
+        instagram: "https://www.instagram.com/igire_rwanda/?hl=en",
+        linkedin: "https://www.linkedin.com/company/shecancodeschool/posts/?feedView=all",
+        github: "#",
+        twitter: "#",
+        personalSite: "#",
+    }
+};
+export type SiteConfig = typeof siteConfig

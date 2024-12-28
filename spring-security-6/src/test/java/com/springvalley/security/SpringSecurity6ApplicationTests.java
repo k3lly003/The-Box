fa2d9a68640c6c14aae5482e4d70cdcda07e5a05
k3lly003 <ntirukelly@gmail.com>
2024-12-28 15:55:01 +0200

    ch(spring-sec-setup)

---
+++ spring-security-6/src/test/java/com/springvalley/security/SpringSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springvalley.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

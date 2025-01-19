5cb1c2ae36daecd6bf6c59e1ce255fe0001e3a59
k3lly003 <ntirukelly@gmail.com>
2025-01-19 09:27:09 +0200

    ft(zuIntro)understanding how zustand works in depth

---
+++ zumenia/zuIntro/src/store.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+type CounterStore = {
+    count: number;
+    increment: ()=> void;
+    incrementAsync: () => Promise<void>;
+    decrement: ()=> void;
+}
+
+// SET / CHANGE / MODIFING THE STATE GOES HERE
+export const useCounterStore = create<CounterStore>((set)=>({
+    count:0,
+    increment: ()=> {set((state) => ({count: state.count + 1}))},
+    incrementAsync: async () => {
+        await new Promise((resolve)=> setTimeout(resolve, 1000));
+        set((state) => ({count: state.count + 1}));
+    },
+    decrement: ()=> {set((state)=>({count: state.count -1 }))},
+}));
